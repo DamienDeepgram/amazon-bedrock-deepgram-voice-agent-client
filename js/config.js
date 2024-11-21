61905cf4ee19233e48563dbae5f0a0ab43f1049c
DamienDeepgram <138720050+DamienDeepgram@users.noreply.github.com>
2024-11-21 11:13:14 -0800

    remove buffer_size

--- js/config.js
+++ js/config.js
@@ -17,7 +17,6 @@ const baseConfig = {
             encoding: "linear16",
             sample_rate: 48000,
             container: "none",
-            buffer_size: 250,
           }
     },
     agent: {
